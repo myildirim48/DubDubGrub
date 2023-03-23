7138614f2c191f31f3a2e04b0c13a35363bcbf9d
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-03-23 11:46:41 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by YILDIRIM on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
